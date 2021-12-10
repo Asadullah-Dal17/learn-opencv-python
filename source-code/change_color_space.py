8f04df388a5ce490404496d89d117b9dbf90418f
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-10 15:06:35 +0500

    converting BGR img to RGB

--- source-code/change_color_space.py
+++ source-code/change_color_space.py
@@ -8,3 +8,7 @@ cv.imshow('colored', img)
 cv.imshow('GrayScale', gray_scale)
 cv.waitKey(0)
 
+# changing BGR image to RGB image 
+RGB_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow('RGB_img', RGB_img)
+cv.waitKey(0)
