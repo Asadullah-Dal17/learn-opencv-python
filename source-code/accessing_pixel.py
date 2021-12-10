6570bb57d8409d0c89cb47c6492778b9e01eeb90
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-10 14:31:42 +0500

    accessing the pixel values

---
+++ source-code/accessing_pixel.py
@@ -0,0 +1,18 @@
+import cv2 as cv 
+
+# reading image 
+img = cv.imread('../images/shapes_image.png')
+
+# getting the pixel value x =100, y=100 location in images
+blue,green, red = img[100,100]
+
+print('blue:', blue, 'green:', green, 'red: ',red)
+
+print('------------')
+# Reading 5x5 region of images, from 0 t0 5 pixel(row, column)
+counter =0
+for row in range(3):
+    for column in range(3):
+        counter+=1
+        print(counter, img[row,column], end=' ')
+    print()
