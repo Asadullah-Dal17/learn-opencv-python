04420be767766598a0943bcb08bd4945a3cb56bd
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-12 17:48:12 +0500

    adding blurrd region script

--- source-code/blurring_region.py
+++ source-code/blurring_region.py
@@ -13,3 +13,10 @@ max_y = 400
 ROI  = img [min_y: max_y, min_x: max_x] # selected Region of image,(Region of intererst)
 cv.imshow('ROI', ROI)
 cv.waitKey(0)
+# blurring the region 
+blurred_region = cv.blur(ROI, (33,33))
+cv.imshow('blurred Region', blurred_region)
+cv.waitKey(0)
+img [min_y: max_y, min_x: max_x] = blurred_region
+cv.imshow('blurred_img with region', img)
+cv.waitKey(0)
