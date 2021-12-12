59048a61e69dbfd423d1a36faabd0ce6444b4bb2
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-12 17:55:17 +0500

    edges detection, done

---
+++ source-code/Edges_detection.py
@@ -0,0 +1,12 @@
+import cv2 as cv 
+
+img = cv.imread('../images/shapes_image.png')
+cv.imshow('img', img)
+cv.waitKey(0)
+grayscale_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('gray_scale', grayscale_img)
+cv.waitKey(0)
+# detecting Edges in the gray scale images 
+edges_img = cv.Canny(grayscale_img, 30, 200)
+cv.imshow("edges", edges_img)
+cv.waitKey(0)
