1028a13a9582ab1fa9138b4a207130169eae13b7
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-12 18:06:37 +0500

    adding resize images script

---
+++ source-code/resize_img.py
@@ -0,0 +1,9 @@
+import cv2 as cv 
+
+img = cv.imread('../images/shapes_image.png')
+cv.imshow('img', img)
+cv.waitKey(0)
+
+resized_img = cv.resize(img, None, fx=0.5, fy=0.5, interpolation=cv.INTER_AREA)
+cv.imshow('resized image', resized_img)
+cv.waitKey(0)
