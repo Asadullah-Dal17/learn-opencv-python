35af29baf0944f23407134e112a72a7fe9ebc882
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-12 17:32:49 +0500

    adding Bluring image, script

---
+++ source-code/Bluring_img.py
@@ -0,0 +1,9 @@
+import cv2 as cv 
+
+img =cv.imread('../images/shapes_image.png')
+
+Blurred_img = cv.blur(img, (33,33))
+cv.imshow('img', img)
+cv.waitKey(0)
+cv.imshow('Blurred Image', Blurred_img)
+cv.waitKey(0)
