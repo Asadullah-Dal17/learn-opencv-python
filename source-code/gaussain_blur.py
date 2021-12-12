487ae020214f09493e72ca5ae88517667342595f
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-12 17:27:48 +0500

    adding Gaussain blur

---
+++ source-code/gaussain_blur.py
@@ -0,0 +1,9 @@
+import cv2 as cv 
+
+img =cv.imread('../images/shapes_image.png')
+
+Gaussian_blur = cv.GaussianBlur(img , (33,33), 0)
+cv.imshow('img', img)
+cv.waitKey(0)
+cv.imshow('Gaussain Blur', Gaussian_blur)
+cv.waitKey(0)
