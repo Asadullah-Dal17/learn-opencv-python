21583fd946870a7313b10ab1624d1708cf85d4a9
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-12 18:08:49 +0500

    resizeing the image

--- source-code/resize_img.py
+++ source-code/resize_img.py
@@ -3,7 +3,7 @@ import cv2 as cv
 img = cv.imread('../images/shapes_image.png')
 cv.imshow('img', img)
 cv.waitKey(0)
-
+# reducing the size of img by half  define by fx for width , fy for height
 resized_img = cv.resize(img, None, fx=0.5, fy=0.5, interpolation=cv.INTER_AREA)
 cv.imshow('resized image', resized_img)
 cv.waitKey(0)
