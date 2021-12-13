0fa63924897fb7605a0664e613498164287f5f88
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-13 08:05:47 +0500

    drawing circle on video frame

---
+++ source-code/Draw-Shapes/01-circle_draw.py
@@ -0,0 +1,29 @@
+import cv2 as cv 
+
+# selecting camera by choosing the number 
+CAMERA_NUMBER = 0
+# create camera object 
+cap = cv.VideoCapture(CAMERA_NUMBER)
+
+while True:
+    # getting frame(ret) available or not and frame its self.
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    # Defing the point for circle 
+    x = 100 
+    y =200
+
+    cv.circle(frame, center=(x, y), radius=20, color=(255,255,255), thickness=5)
+    
+    cv.imshow('frame', frame)
+    # define key to close the program
+    key = cv.waitKey(1)
+    if key ==ord('q'):
+        # when 'q' press on Key board the program will quit 
+        break
+# close the camera 
+cap.release()
+# close all the windows
+cv.destroyAllWindows()
