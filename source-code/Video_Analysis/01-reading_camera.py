93cf06e6b15b72601755ba0f40d7fd9deeb092d1
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-13 07:24:56 +0500

    basic Camera loading in

---
+++ source-code/Video_Analysis/01-reading_camera.py
@@ -0,0 +1,21 @@
+import cv2 as cv 
+
+# selecting camera by choosing the number 
+CAMERA_NUMBER = 0
+# create camera object 
+cap = cv.VideoCapture(CAMERA_NUMBER)
+
+while True:
+    # getting frame(ret) available or not and frame its self.
+    ret, frame = cap.read()
+    if not ret:
+        break
+    cv.imshow('frame', frame)
+    # define key to close the program
+    key = cv.waitKey(1)
+    if key ==ord('q'):
+        break
+# close the camera 
+cap.release()
+# close all the windows
+cv.destroyAllWindows()
