744c550b92ed388fb2d9df98fb42b0fed4b69f6f
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-15 07:30:37 +0500

    reading image complete done

--- C_Plus_Plus/Basic/Read_image/main.cpp
+++ C_Plus_Plus/Basic/Read_image/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "opencv2/opencv.hpp"
+using namespace std;
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+
+int main(int argc, char** argv )
+{
+
+    cv::Mat image;  // variable image of datatype Matrix
+    image = cv::imread("C:/Users/Asadullah/Projects/Opencv/Course/coding_pubic/learn-opencv-python/images/shapes_image.png");
+
+    cv::imshow("Display Image", image);
+    cv::waitKey(0);
+    return 0;
 }
