de49ac2da7c46ebd6d3ade447e2e7f6793f82d6d
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-15 08:27:53 +0500

    updating file

--- C_Plus_Plus/Basic/Read_image/main.cpp
+++ C_Plus_Plus/Basic/Read_image/main.cpp
@@ -7,7 +7,6 @@ int main(int argc, char** argv )
 
     cv::Mat image;  // variable image of datatype Matrix
     image = cv::imread("C:/Users/Asadullah/Projects/Opencv/Course/coding_pubic/learn-opencv-python/images/shapes_image.png");
-
     cv::imshow("Display Image", image);
     cv::waitKey(0);
     return 0;
