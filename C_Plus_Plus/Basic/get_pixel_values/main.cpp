237ad31ad3481a19d52686ce005d31504205cbca
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-16 17:58:12 +0500

    reading image from dir

--- C_Plus_Plus/Basic/get_pixel_values/main.cpp
+++ C_Plus_Plus/Basic/get_pixel_values/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+    cv:: Mat Image ;
+    Image = cv::imread("C:/Users/Asadullah/Projects/Opencv/Course/coding_pubic/learn-opencv-python/images/shapes_image.png");
+    
+    cv::imshow("image", Image);
+    cv::waitKey(0);
+    return 0;
+
 }
