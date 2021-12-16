7795226deee4c5ecfd1999882b681007a9c9a266
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-16 18:43:58 +0500

    accessing pixels complete 😀

--- C_Plus_Plus/Basic/get_pixel_values/main.cpp
+++ C_Plus_Plus/Basic/get_pixel_values/main.cpp
@@ -3,9 +3,19 @@
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
-    cv:: Mat Image ;
+    cv:: Mat Image;
     Image = cv::imread("C:/Users/Asadullah/Projects/Opencv/Course/coding_pubic/learn-opencv-python/images/shapes_image.png");
-    
+    // getting the pixel values 
+    int b = Image.at<cv::Vec3b>(10, 10)[0];
+    int g = Image.at<cv::Vec3b>(10, 10)[1];
+    int r = Image.at<cv::Vec3b>(10, 10)[2];
+
+    std::cout<<"BLUE values  "<<b<<std::endl;
+    std::cout<<"GREEN values  "<<g<<std::endl;
+    std::cout<<"RED values  "<<r<<std::endl;
+    std::cout<<"-------- \n --------- \n -------- \n --- ";
+
+
     cv::imshow("image", Image);
     cv::waitKey(0);
     return 0;
