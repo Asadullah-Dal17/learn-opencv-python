81e63d8c1a967cf7f18106cfe78223e9eae65808
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-17 08:59:01 +0500

    updating file

--- C_Plus_Plus/Basic/03-setting_pixel_values/main.cpp
+++ C_Plus_Plus/Basic/03-setting_pixel_values/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
-
+#include <opencv2/opencv.hpp>
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+
+std::string img_path = "C:/Users/Asadullah/Pictures/color_Square.png";
+cv::Mat original_img;
+
+original_img = cv::imread(img_path);
+
+cv::Mat modifed_img = cv::imread(img_path);
+for (int r=0; r<modifed_img.rows; r++){
+    // std::cout<<" "<<r;
+    for(int c =0; c<modifed_img.cols; c++){
+        
+    std::cout<<" "<<c;
+
+    }
+}
+cv::imshow("original_img", original_img);
+cv::imshow("modified_img", modifed_img);
+cv::waitKey();
+
+
 }
