32d0aa6865e35893c84165d940ea912c4a0ae0f3
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-17 09:07:26 +0500

    modfiying the pixel values

--- C_Plus_Plus/Basic/03-setting_pixel_values/main.cpp
+++ C_Plus_Plus/Basic/03-setting_pixel_values/main.cpp
@@ -5,14 +5,14 @@ int main(int, char**) {
 std::string img_path = "C:/Users/Asadullah/Pictures/color_Square.png";
 cv::Mat original_img;
 
-original_img = cv::imread(img_path);
+original_img = cv::imread(img_path, cv::IMREAD_COLOR);
 
-cv::Mat modifed_img = cv::imread(img_path);
+cv::Mat modifed_img = cv::imread(img_path, cv::IMREAD_COLOR);
 for (int r=0; r<modifed_img.rows; r++){
     // std::cout<<" "<<r;
     for(int c =0; c<modifed_img.cols; c++){
         
-    std::cout<<" "<<c;
+    modifed_img.at<cv::Vec3b>(r, c)[0] = modifed_img.at<cv::Vec3b>(r ,c)[0] =0;
 
     }
 }
