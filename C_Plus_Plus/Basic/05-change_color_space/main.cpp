92d26e9df3649cfb0a1d7f30735761a828f1690d
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-19 15:17:46 +0500

    change color space c++

--- C_Plus_Plus/Basic/05-change_color_space/main.cpp
+++ C_Plus_Plus/Basic/05-change_color_space/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 int main(int, char**) {
-    // std::cout << "Hello, world!\n";
+    // creating the image path 
     std::string img_path ="C:/Users/Asadullah/Pictures/color_Square.png";
+    // creating the empty image 
     cv::Mat img;
     img = cv::imread(img_path);
     // creating Mat for gray scale image 
-    cv::Mat gray ;
+    cv::Mat gray;
+    // converting the color image to gray scale image 
     cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    // displaying the image using imshow function from opencv python 
     cv::imshow("gray", gray);
     cv::imshow("img", img);
     cv::waitKey();
