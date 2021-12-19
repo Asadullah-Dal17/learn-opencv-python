494e07bc598bb49015c12fb8710bf06b30dd41d7
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-19 17:53:29 +0500

    bluring the region is complete

--- C_Plus_Plus/essential_medthods/03-Bluring_Region/main.cpp
+++ C_Plus_Plus/essential_medthods/03-Bluring_Region/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
-
+#include <opencv2/opencv.hpp>
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+
+    cv::Mat img, region, blurred_region;
+    // region points
+     int x, y , width, height ; 
+    x =140; y =216; width = 240; height =200;
+
+    img = cv::imread("C:/Users/Asadullah/Pictures/Lenna.png");
+    cv::Rect crop_region(x, y, width, height );
+    region = img(crop_region);
+    cv::blur(region, blurred_region, cv::Size(13,13));
+    cv::imshow("blured_region",blurred_region);
+    cv::imshow("Region", region);
+    cv::imshow("img", img);
+    cv::waitKey();
+
 }
