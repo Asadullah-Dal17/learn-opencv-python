908d56ce541a7d0d380904459dbccbb04f353f08
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-19 17:27:17 +0500

    blur medthod is completed

--- C_Plus_Plus/essential_medthods/02-Blur/main.cpp
+++ C_Plus_Plus/essential_medthods/02-Blur/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
-
+#include <opencv2/opencv.hpp>
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    // std::cout << "Hello, world!\n";
+    // reading image from dir 
+    std::string path_img = "C:/Users//Asadullah/Pictures/Lenna.png";
+    cv:: Mat img ;
+    cv:: Mat blured, blured2;
+    img =cv::imread(path_img);
+    // bluring the image 
+    cv::blur(img, blured, cv::Size(13,13));
+    cv::blur(img, blured2, cv::Size(33,33));
+
+    cv::imshow("blured image", blured);
+    cv::imshow("blured2 image", blured2);
+
+
+    cv::imshow("img", img);
+    cv::waitKey();
 }
