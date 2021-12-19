2bb9cba698c9ca8fdbeb8f7075b418f9fc8fe359
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-19 14:48:25 +0500

    upating file

--- C_Plus_Plus/Basic/create_empyt_image/main.cpp
+++ C_Plus_Plus/Basic/create_empyt_image/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+#include <opencv2/opencv.hpp>
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    // cv::Mat img ;
+    std::cout<<"done";
+    int width =640;
+    int height =480;
+    // cv::Scalar color  = (0,255,0);
+    cv::Mat img(height, width, CV_8UC3, cv::Scalar(0,0,0));
+
+    cv::imshow("img", img);
+    cv::waitKey();
 }
