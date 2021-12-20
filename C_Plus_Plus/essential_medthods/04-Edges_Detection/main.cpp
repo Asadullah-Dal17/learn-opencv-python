3fe72221316c0948f3588ee12ade9d907f0a2944
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-20 09:47:34 +0500

    done with Edges detection

--- C_Plus_Plus/essential_medthods/04-Edges_Detection/main.cpp
+++ C_Plus_Plus/essential_medthods/04-Edges_Detection/main.cpp
@@ -4,8 +4,14 @@
 int main(int, char**) {
 
     // declearing the variables
-    cv::Mat img, edges, gray;
+    cv::Mat img, edges, gray, blur_img;
+    
     img = cv::imread("C:/Users/Asadullah/Pictures/Lenna.png");
+    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    cv::GaussianBlur(gray, blur_img, cv::Size(5,5),0);
+    cv::Canny(blur_img, edges, 40, 200, 3, false);
+    cv::imshow("Canny", edges);
+
     cv::imshow("img", img);
     cv::waitKey();   
 
