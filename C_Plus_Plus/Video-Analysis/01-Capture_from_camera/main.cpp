a6f5638cfbcf20db83dbbe5114582c7652445743
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-20 15:50:49 +0500

    complete camera capture

--- C_Plus_Plus/Video-Analysis/01-Capture_from_camera/main.cpp
+++ C_Plus_Plus/Video-Analysis/01-Capture_from_camera/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
-
+#include <opencv2/opencv.hpp>
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    cv::VideoCapture cap (3);
+    
+if (cap.isOpened() ==false){
+    std::cout<<"can't open the video"<<std::endl;
+    return -1;
+}
+while (true){
+    cv::Mat frame ;
+    bool ret = cap.read(frame);
+   if ( ret ==false){
+       std::cout<<"Found the end of Video"<<std::endl;
+       break;
+   }
+
+   cv::imshow("frame", frame);
+   if (cv::waitKey(1)==27){
+       std::cout<<" close the Video Stream";
+       break;
+   }
+
+}
+
 }
