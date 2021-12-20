44eeb29b26f57f5b9826bd340ff97dc92c4d420f
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-20 11:53:35 +0500

    complete Resizeing the imge

--- C_Plus_Plus/essential_medthods/05-Resize_image/main.cpp
+++ C_Plus_Plus/essential_medthods/05-Resize_image/main.cpp
@@ -2,10 +2,15 @@
 #include <opencv2/opencv.hpp>
 
 int main(int, char**) {
-    cv::Mat img, resize;
-    // int width = 300; 
-    // int height = 300;
+    cv::Mat img, resized_img;
+    int width = 300; 
+    int height = 300;
     std::string path_img = "C:/Users/Asadullah/Pictures/Lenna.png";
+    // reszing the image 
+    img = cv::imread(path_img);
+    cv::resize(img, resized_img, cv::Size( width, height), cv::INTER_LINEAR);
+    cv::imshow("Resized Img", resized_img);
+
     img = cv::imread(path_img);
     cv::imshow("img", img);
     cv::waitKey();
