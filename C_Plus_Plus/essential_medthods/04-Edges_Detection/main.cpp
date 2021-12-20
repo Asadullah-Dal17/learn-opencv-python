f6101dc630ab21dd2b6b7a150ece61a3fdb9355e
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-20 09:20:40 +0500

    set up project

--- C_Plus_Plus/essential_medthods/04-Edges_Detection/main.cpp
+++ C_Plus_Plus/essential_medthods/04-Edges_Detection/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+
+    // declearing the variables
+    cv::Mat img, edges, gray;
+    img = cv::imread("C:/Users/Asadullah/Pictures/Lenna.png");
+    cv::imshow("img", img);
+    cv::waitKey();   
+
 }
